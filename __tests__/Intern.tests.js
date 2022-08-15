8e2ddbebc522852471a237bfb71fd195edd27a44
devihall <devis0306@gmail.com>
2022-08-15 19:36:23 -0400

    html pade complete

--- __tests__/Intern.tests.js
+++ __tests__/Intern.tests.js
@@ -1,5 +1,5 @@
 const Intern = require("../lib/Intern");
-const intern = new Intern("devi", "12345",  "Univ of M");
+const intern = new Intern("devi", "12345", "d.hall@gmail.com", "Univ of M");
 
 test("getting constructor values for intern object", () => {
   expect(intern.name).toBe("devi");
@@ -20,8 +20,8 @@ test("test to get the email of intern with getEmail() method", () => {
   expect(intern.getEmail()).toBe("d.hall@gmail.com");
 });
 
-test("test to get the email of intern with getSchool() method", () => {
-  expect(intern.getSchool()).toBe("d.hall@gmail.com");
+test("test to get the school of intern with getSchool() method", () => {
+  expect(intern.getSchool()).toBe("Univ of M");
 });
 
 test("test to get the role of intern with getRole() method", () => {
